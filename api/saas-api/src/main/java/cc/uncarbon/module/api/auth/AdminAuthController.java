6bec01fcb448a5cd119c5ab70f5363dd187c0a76
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2021-12-26 13:42:00 +0800

    docs: 使用常量替代硬编码

--- api/saas-api/src/main/java/cc/uncarbon/module/api/auth/AdminAuthController.java
+++ api/saas-api/src/main/java/cc/uncarbon/module/api/auth/AdminAuthController.java
@@ -64,7 +64,7 @@ public class AdminAuthController {
 
         // 注册到SA-Token
         AdminStpUtil.login(userInfo.getId(), dto.getRememberMe());
-        AdminStpUtil.getSession().set("userContext", userContext);
+        AdminStpUtil.getSession().set(UserContext.CAMEL_NAME, userContext);
 
 
         // 返回登录token
